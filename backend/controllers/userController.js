9c9783d786e1b522fe1fc1b136b8c5a822631a97
Srujan <srujan@Srujans-MacBook-Air.local>
2022-10-25 18:19:28 +0530

    added routes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,13 @@
+const registerUser = (req,res) => {
+    res.send("register route")
+}
+
+const loginUser = (req,res) => {
+    res.send("login route")
+}
+
+
+module.exports ={
+    registerUser,
+    loginUser
+}
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log("hai srujan")
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const PORT = process.env.PORT || 5000
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.json({message: 'hai srujan'})
+})
+
+app.use('/api/users',require('./routes/userRoutes'))
+
+app.listen(PORT, ()=>{
+    console.log(`listening on port ${PORT}`);
+})
