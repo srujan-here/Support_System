39516e1ad2067826b5d8f7721367e6bbaa557711
Srujan <srujan@Srujans-MacBook-Air.local>
2022-11-19 18:19:16 +0530

    frontend chages

--- frontend/src/features/auth/authSllice.js
+++ frontend/src/features/auth/authSllice.js
@@ -1,34 +1,31 @@
 import { createSlice, createAsyncThunk, createAction } from "@reduxjs/toolkit";
 
 const initialState = {
- user:"srujan",
- isError:false,
- isSuccess:false,
- isLoading:false,
- message:'',
+  user: "srujan",
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
 };
 
 //register
-export const register =createAsyncThunk('auth/register',async (user,thunkAPI)=>{
-
-  console.log(user)
-
-})
+export const register = createAsyncThunk(
+  "auth/register",
+  async (user, thunkAPI) => {
+    console.log(user);
+  }
+);
 
 //login
 
-export const login =createAsyncThunk('auth/login',async (user,thunkAPI)=>{
-
-  console.log(user)
-
-})
+export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+  console.log(user);
+});
 
 export const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {
-    
-  },
+  reducers: {},
   extraReducers: (builder) => {},
 });
 
